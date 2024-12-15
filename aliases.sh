97003b1f32631fc7496ad6f59ef08afd052bd6f5
Zilezia <czeriwanczak@gmail.com>
2024-12-15 16:23:34 +0000

    upd 15/12/24

--- aliases.sh
+++ aliases.sh
@@ -8,8 +8,8 @@ alias upd='sudo apt update'
 alias upg='sudo apt upgrade'
 
 # interesting
-alias fuck='reboot now'
-alias FUCK='shutdown now'
+alias fuck='sudo reboot now'
+alias FUCK='sudo shutdown now'
 
 alias snapi='sudo snap install "$1"'
 alias apti='sudo apt install "$1"'
@@ -17,11 +17,29 @@ alias tarr='tar -xvf "$1"'
 alias mkcd='mkdir -p -- "$1" && cd -P -- "$1"'
 alias rmd='rm -rf "$1"'
 
+#alias reloadx="x-terminal-emulator && wmctrl -r ':ACTIVE:' -b toggle,fullscreen"
 alias reload='source ~/.bashrc'
 alias cd.='cd ..'
 alias cls='clear' # lol
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
-
 #alias alert='notify-send --urgency=low -i "([ $? = 0 ] && echo terminal || echo error)" "$(history|tail)"'
+
+# git
+# ofc this hasnt worked, gotta make a function
+# alias gush='git add . && git commit -m "\"$1\"" && git push'
+
+# MySQL/SQL
+alias rsql='mysql -u root -p' # no sudo to avoid double password typing
+
+# Rust
+alias carr='cargo r'
+alias carR='cargo r -r'
+alias carc='cargo c'
+alias carC='cargo clean'
+
+# Nicher, Trunk
+alias trunb='trunk build'
+alias trunB='trunk build --release'
+alias trunc='trunk clean'
--- functions/nalias.sh
+++ functions/nalias.sh
@@ -1,25 +1,65 @@
 # 'Auto' create bash aliases
 
 nalias() {
-
-    help() {
+    # Help message
+    if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
         echo "nalias <commands>"
         echo "Options:"
         echo "  -h, --help      Display this help message"
         echo "  -a              Does nothing"
-    }
-    if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then help; return 0; fi
+        return 0
+    fi
 
-    while getopts "a" opt; do
+    alias_name=''
+    alias_func=''
+    line_write=28
+
+     while getopts ":n:f:L::" opt; do
+        echo "Proc opt: $opt, OPTARG: $OPTARG"
         case $opt in
-            a)
-                echo "a"
-                return 0;;
+            n)
+                if [ -n "$OPTARG" ]; then
+                    echo "$OPTARG"
+                    alias_name="$OPTARG"
+                else
+                    echo "Error: Option -n requires an argument."
+                    return -1
+                fi;;
+            f)
+                if [ -n "$OPTARG" ]; then
+                    echo "$OPTARG"
+                    alias_func="$OPTARG"
+                else
+                    echo "Error: Option -f requires an argument."
+                    return -1
+                fi;;
+            L)
+                if [ -n "$OPTARG" ]; then
+                    echo "$OPTARG"
+                    line_write="$OPTARG"
+                fi;;
+            \?)
+                echo "Invalid option: -$OPTARG"
+                return -1;;
             *)
-                echo "$1 is not recognised"
-                return 0;;
+                echo "Option: -$OPTARG requires an argument."
+                return -1;;
         esac
     done
+    echo "name: $alias_name, func: $alias_func, line: $line_write"
+   #if [ -z "$alias_name" ] || [ -z "$alias_func" ]; then
+   #    echo "Empty"
+   #    alias_name=''
+   #    alias_func=''
+   #    return 0
+   #fi
+
+    text_write="alias $alias_name='$alias_func'"
+    #sed -i "${line_write}i\\$text_write" ~/.bash/aliases.sh
+
+    #reload
+    echo "$text_write"
+    return 1
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then nalias "$@"; fi
