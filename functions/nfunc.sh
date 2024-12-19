38652206b8883f2c94ef4cc25c1a5937a6ca6194
Zilezia <czeriwanczak@gmail.com>
2024-12-19 15:12:39 +0000

    Automated commit

--- functions/nfunc.sh
+++ functions/nfunc.sh
@@ -1,18 +1,40 @@
-# TODO this
+# TODO still doing this, but initialising works
+
 # 'Auto' create bash functions faster instead of writing them out yourself (like this)
 
 nfunc() {
-    help() {
+    if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
         echo "nfunc - Create new bash function"
         echo "Options:"
-        echo "  -h, --help      Display this help message"
+        return
+    fi
+
+    # check maybe if the function already exists?
+
+    local func_name=${1%.*}
+    local file_name="$func_name.sh"
+    local file_path="$bash/functions/$file_name"
+
+    local func_desc="n/a"
+
+cat > "$file_path" << EOF
+$func_name() {
+    help() {
+        echo "$func_name - $func_desc"
+        echo "Options:"
         return
     }
-    if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then help; return 0; fi
+    if [ "\$1" = "-h" ] || [ "\$1" = "--help" ]; then help; return 0; fi
 
-    local file=${1%.*}
-    local file_name="$file.sh"
-    local file_path="$bash/functions/$file_name"
+    echo "test func used!"
+    return
+}
+
+if [[ "\${BASH_SOURCE[0]}" == "\${0}" ]]; then $func_name "\$@"; fi
+EOF
+
+    reload
+    nano "$file_path"
     return
 }
 
--- profile.sh
+++ profile.sh
@@ -1,12 +1,16 @@
 
 # bash
 export bash=~/.bash
-export bafunc
+export bafunc=$bash/functions
 
 # Projects dir
 export Kode=~/Kode
 ## Projects
-export zilezia_dev=~/Kode/zilezia.dev
-export site=~/Kode/zilezia.dev
+export zilezia_dev=$Kode/zilezia.dev
+export site=$zilezia_dev
 
-export pet_crem=~/Kode/pet_crem
+export pet_crem=$Kode/pet_crem
+
+export RoPC=$Kode/RoPC_plugin
+export ropc=$RoPC
+
