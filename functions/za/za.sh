0e7c1cf8b1ed3f461b1e252f293c650a0327bac1
Zilezia <czeriwanczak@gmail.com>
2025-01-18 02:00:01 +0000

    Automated commit

--- functions/za/za.sh
+++ functions/za/za.sh
@@ -1,18 +1,24 @@
 _za() {
-    local cur opts opts_short
+    local cur
+    local stands
+    local opts opts_short
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
+    stands="warudo"
     opts="      --edit --help"
     opts_short=" -e     -h"
 
-    COMPREPLY=(  $(compgen -W "${opts}"       -- ${cur}) )
+    COMPREPLY=(  $(compgen -W "${stands}"     -- ${cur}) )
+    COMPREPLY+=( $(compgen -W "${opts}"       -- ${cur}) )
     COMPREPLY+=( $(compgen -W "${opts_short}" -- ${cur}) )
     return
 }
 
 za() {
     help() {
-        echo "za <options>"
+        echo "za [stand] <options>"
+     echo -e "\nStands:"
+        echo "  warudo          Stop everything briefly"
      echo -e "\nOptions:"
         echo "  -e, --edit      Edit source code"
         echo "  -h, --help      Display this help message"
@@ -21,7 +27,16 @@ za() {
 
     func() {
         #mpg123 ~/Downloads/za_warudo.mp3
-        play ~/Downloads/za_warudo.mp3 -q -t alsa
+        #nohup play ~/Downloads/za_warudo.mp3 -q -t alsa > /dev/null 2>&1 & disown
+        (play ~/Downloads/za_warudo.mp3 -q -t alsa &)
+        sleep 1 # takes time to say the words
+        echo "stopping everything.."
+        local count=10
+        for i in $(seq $count); do
+            echo "$is"
+            sleep 1
+        done
+        echo "time resume"
     }
 
     case $1 in
